b39642e5e638b23ea00016e962093119e9faee08
Jiook Cha <cha.jiook@gmail.com>
2018-02-26 10:34:32 -0500

    Update recon_1.sh

--- recon_1.sh
+++ recon_1.sh
@@ -6,26 +6,30 @@ list=list_t1_batch${1}.txt
 echo $list
 threads=64
 
-abcd=/lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/
+abcd=/global/cscratch1/sd/jcha9928/anal/ABCD/
 
-CMD_batch=/lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/abcd_alcf_code/job/cobaltjob.recon.batch${1}
+CMD_batch=/global/cscratch1/sd/jcha9928/anal/ABCD/abcd_nersc_code/job/cobaltjob.recon.batch${1}
 rm -rf $CMD_batch
 
 ####################################################################################
 cat<<EOA >$CMD_batch
 #!/bin/bash
-#COBALT -t 720
-#COBALT -n 1000
-#COBALT --attrs mcdram=cache:numa=quad
-#COBALT -A AD_Brain_Imaging
-#COBLAT -M jiook.cha@nyspi.columbia.edu
+#SBATCH -N 1024
+#SBATCH -C haswell
+#SBATCH -q regular
+#SBATCH -J recon
+#SBATCH --mail-user=jiook.cha@nyspi.columbia.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 24:00:00
+#SBATCH -L cscratch1
+
+#OpenMP settings:
+export OMP_NUM_THREADS=64
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
 
 echo start............................................
-#export n_nodes=$COBALT_JOBSIZE
-#export n_mpi_ranks_per_node=202
-#export n_mpi_ranks=202
-#export n_openmp_threads_per_rank=64
-#export n_hyperthreads_per_core=4
+
 EOA
 #####################################################################
 
@@ -33,10 +37,10 @@ EOA
 
 
 i=1
-for s in `cat /lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/abcd_alcf_code/\$list`
+for s in `cat /global/cscratch1/sd/jcha9928/anal/ABCD/abcd_nersc_code/\$list`
 do
 #s=`echo $SUBJECT | egrep -o '[0-9]{8}'`
-CMD=/lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/abcd_alcf_code/job/cmd.recon.${s}
+CMD=/global/cscratch1/sd/jcha9928/anal/ABCD/abcd_nersc_code/job/cmd.recon.${s}
 rm -rf $CMD
 
 #CMD_sub=/lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/abcd_alcf_code/job/cmd_sub.recon.${s}
@@ -44,7 +48,7 @@ rm -rf $CMD
 
 SUBJECT=${s}
 echo ${SUBJECT}
-datafolder=/lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/data
+datafolder=/global/cscratch1/sd/jcha9928/anal/ABCD/data
 t1=${datafolder}/${s}/ses-baselineYear1Arm1/anat/${s}_ses-baselineYear1Arm1_T1w.nii.gz
 t2=${datafolder}/${s}/ses-baselineYear1Arm1/anat/${s}_ses-baselineYear1Arm1_T2w.nii.gz
   if [ ! -e $t2 ]; then t2_arg=" "
@@ -58,10 +62,10 @@ t2=${datafolder}/${s}/ses-baselineYear1Arm1/anat/${s}_ses-baselineYear1Arm1_T2w.
 #############################################CMD#####################################
 cat<<EOC >$CMD
 #!/bin/bash
-source ~/.bashrc
-FREESURFER_HOME=/lus/theta-fs0/projects/AD_Brain_Imaging/app/freesurfer
+source ~/.bashrc_jiook
+FREESURFER_HOME=/global/homes/j/jcha9928/app/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR=/lus/theta-fs0/projects/AD_Brain_Imaging/anal/ABCD/fs
+SUBJECTS_DIR=/global/cscratch1/sd/jcha9928/anal/ABCD/fs
 ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=64
 
 recon-all -all -s ${SUBJECT} -i ${t1} ${t2_arg} ${hippo_arg} -parallel -openmp 64
@@ -72,7 +76,8 @@ EOC
 
 chmod +x $CMD
 
-echo "aprun -n 1 -N 1 -d 64 -j 1 -cc depth -e OMP_NUM_THREADS=64 $CMD > ./job/log.recon.${SUBJECT} 2>&1 &">>$CMD_batch 
+#echo "aprun -n 1 -N 1 -d 64 -j 1 -cc depth -e OMP_NUM_THREADS=64 $CMD > ./job/log.recon.${SUBJECT} 2>&1 &">>$CMD_batch 
+echo "srun -N 1 -n 1 -c 64 --cpu_bind=cores $CMD > ./job/log.recon.${SUBJECT} 2>&1">>$CMD_batch
 echo "sleep 0.5">>$CMD_batch
 
 i=$(($i+1))
@@ -85,6 +90,8 @@ echo "wait" >> $CMD_batch
 
 echo $CMD_batch
 chmod +x $CMD_batch
-qsub $CMD_batch
+#qsub $CMD_batch
+
+
 #$code/fsl_sub_hpc_2 -s smp,$threads -l /ifs/scratch/pimri/posnerlab/1anal/adni/adni_on_c2b2/job -t $CMD_batch
 #$code/fsl_sub_hpc_6 -l /ifs/scratch/pimri/posnerlab/1anal/adni/adni_on_c2b2/job -t $CMD_batch
