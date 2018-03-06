f6a2eb10e7685c0690fccf27d5ff00de23e09d7c
Jiook Cha <cha.jiook@gmail.com>
2018-03-05 21:32:46 -0500

    Update recon_1.sh

--- recon_1.sh
+++ recon_1.sh
@@ -105,7 +105,7 @@ echo "wait" >> $CMD_batch
 
 echo $CMD_batch
 chmod +x $CMD_batch
-sbatch $CMD_batch
+echo sbatch $CMD_batch
 
 
 #$code/fsl_sub_hpc_2 -s smp,$threads -l /ifs/scratch/pimri/posnerlab/1anal/adni/adni_on_c2b2/job -t $CMD_batch
