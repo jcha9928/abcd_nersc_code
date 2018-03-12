7c59aef0b353afd4ba3bf62a3f5381fe2e3df9b5
Jiook Cha <cha.jiook@gmail.com>
2018-03-11 21:28:25 -0400

    Update recon_1.sh

--- recon_1.sh
+++ recon_1.sh
@@ -18,7 +18,7 @@ cat<<EOA >$CMD_batch
 
 #SBATCH -N $N
 #SBATCH -C haswell
-#SBATCH -q regular
+#SBATCH -q premium
 #SBATCH -J recon
 #SBATCH --mail-user=jiook.cha@nyspi.columbia.edu
 #SBATCH --mail-type=ALL
